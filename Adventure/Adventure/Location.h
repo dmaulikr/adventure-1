3ac5b24d675e8ce6c34b71a8d8f8a5922e536ed4
akawalsky <kawalsky.aaron@gmail.com>
2012-12-16 18:52:08 -0800

    core data start

---
+++ Adventure/Adventure/Location.h
@@ -0,0 +1,13 @@
+//
+//  Location.h
+//  Adventure
+//
+//  Created by Aaron Kawalsky on 12/16/12.
+//  Copyright (c) 2012 Tal Levy. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface Location : NSManagedObject
+
+@end
---
+++ Adventure/Adventure/Location.m
@@ -0,0 +1,13 @@
+//
+//  Location.m
+//  Adventure
+//
+//  Created by Aaron Kawalsky on 12/16/12.
+//  Copyright (c) 2012 Tal Levy. All rights reserved.
+//
+
+#import "Location.h"
+
+@implementation Location
+
+@end
