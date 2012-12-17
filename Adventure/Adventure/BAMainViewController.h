bba6db62cdf66ea54e158b13e1fbf19bdbe4e649
akawalsky <kawalsky.aaron@gmail.com>
2012-12-16 17:01:51 -0800

    Added Files

---
+++ Adventure/Adventure/BAMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  BAMainViewController.h
+//  Adventure
+//
+//  Created by Aaron Kawalsky on 12/16/12.
+//  Copyright (c) 2012 Tal Levy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BAMainViewController : UIViewController
+
+@end
---
+++ Adventure/Adventure/BAMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  BAMainViewController.m
+//  Adventure
+//
+//  Created by Aaron Kawalsky on 12/16/12.
+//  Copyright (c) 2012 Tal Levy. All rights reserved.
+//
+
+#import "BAMainViewController.h"
+
+@interface BAMainViewController ()
+
+@end
+
+@implementation BAMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
